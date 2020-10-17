9a15700c3174a9d1d579ff2d8101cceb69d1d3a9
Mike Krinkin <krinkin.m.u@gmail.com>
2020-10-17 18:51:01 +0100

    Output EFI GUIDs of protocols supported on the app image handle
    This functionality is useful to explore what handles support what
    protocols. I want to try to implement a simple logic of finding a file
    in the same directory as our binary and load it in memory.
    
    I assume that we should be able to use the handle passed to the entry
    point to figure out the file path, which would allow us to find the
    right directory path.
    
    Also via the handle passed to the entry point we should be able to have
    access to the volume where the file is located. With that we can use
    UEFI File APIs to read the file in memory.
    
    The fundamental idea of this exercesize is that loading and OS image is
    basically loading file in memory and the passing control to the right
    place in memory. So it should be a simple enough demostration of how an
    EFI based OS loader could work.

--- efi.h
+++ efi.h
@@ -8,6 +8,13 @@ typedef void *efi_handle_t;
 typedef uint64_t efi_status_t;
 typedef uint64_t efi_uint_t;
 
+struct efi_guid {
+	uint32_t data1;
+	uint16_t data2;
+	uint16_t data3;
+	uint8_t data4[8];
+};
+
 struct efi_table_header {
 	uint64_t signature;
 	uint32_t revision;
@@ -17,38 +24,95 @@ struct efi_table_header {
 };
 
 struct efi_simple_text_output_protocol {
-	efi_status_t (*unused1)(
-			struct efi_simple_text_output_protocol *,
-			bool);
+	void (*unused1)();
 
 	efi_status_t (*output_string)(
 			struct efi_simple_text_output_protocol *,
 			uint16_t *);
 
-	efi_status_t (*unused2)(
-			struct efi_simple_text_output_protocol *,
-			uint16_t *);
-	efi_status_t (*unused3)(
-			struct efi_simple_text_output_protocol *,
-			efi_uint_t, efi_uint_t *, efi_uint_t *);
-	efi_status_t (*unused4)(
-			struct efi_simple_text_output_protocol *,
-			efi_uint_t);
-	efi_status_t (*unused5)(
-			struct efi_simple_text_output_protocol *,
-			efi_uint_t);
+	void (*unused2)();
+	void (*unused3)();
+	void (*unused4)();
+	void (*unused5)();
 
 	efi_status_t (*clear_screen)(
 			struct efi_simple_text_output_protocol *);
 
-	efi_status_t (*unused6)(
-			struct efi_simple_text_output_protocol *,
-			efi_uint_t, efi_uint_t);
-	efi_status_t (*unused7)(
-			struct efi_simple_text_output_protocol *,
-			bool);
+	void (*unused6)();
+	void (*unused7)();
 
 	void *unused8;
+};
+
+struct efi_boot_services {
+	struct efi_table_header header;
+
+	// Task Priority Services
+	void (*unused1)();
+	void (*unused2)();
+
+	// Memory Services
+	void (*unused3)();
+	void (*unused4)();
+	void (*unused5)();
+	void (*unused6)();
+	efi_status_t (*free_pool)(void *);
+
+	// Event & Timer Services
+	void (*unused7)();
+	void (*unused8)();
+	void (*unused9)();
+	void (*unused10)();
+	void (*unused11)();
+	void (*unused12)();
+
+	// Protocol Handler Services
+	void (*unused13)();
+	void (*unused14)();
+	void (*unused15)();
+	void (*unused16)();
+	void *reserved;
+	void (*unused17)();
+	void (*unused18)();
+	void (*unused19)();
+	void (*unused20)();
+
+	// Image Services
+	void (*unused21)();
+	void (*unused22)();
+	void (*unused23)();
+	void (*unused24)();
+	void (*unused25)();
+
+	// Miscellaneius Services
+	void (*unused26)();
+	void (*unused27)();
+	void (*unused28)();
+
+	// DriverSupport Services
+	void (*unused29)();
+	void (*unused30)();
+
+	// Open and Close Protocol Services
+	void (*unused31)();
+	void (*unused32)();
+	void (*unused33)();
+
+	// Library Services
+	efi_status_t (*protocols_per_handle)(
+		efi_handle_t, struct efi_guid ***, efi_uint_t *);
+	void (*unused35)();
+	void (*unused36)();
+	void (*unused37)();
+	void (*unused38)();
+
+	// 32-bit CRC Services
+	void (*unused39)();
+
+	// Miscellaneius Services (cont)
+	void (*unused40)();
+	void (*unused41)();
+	void (*unused42)();
 };
 
 struct efi_system_table {
@@ -62,7 +126,7 @@ struct efi_system_table {
 	void *unused6;
 	void *unused7;
 	void *unused8;
-	void *unused9;
+	struct efi_boot_services *boot;
 	efi_uint_t unused10;
 	void *unused11;
 };
--- main.c
+++ main.c
@@ -1,16 +1,105 @@
 #include "efi.h"
 
+static uint16_t *u64_to_str(uint64_t x, uint16_t *buf)
+{
+	uint16_t *pos = buf;
+	uint16_t *begin;
+	uint16_t *end;
+
+	*pos++ = u'0';
+	*pos++ = u'x';
+	begin = pos;
+	do {
+		if ((x & 0xf) < 10) *pos++ = u'0' + (x & 0xf);
+		else *pos++ = u'A' + (x & 0xf) - 10;
+		x >>= 4;
+	} while (x);
+	end = pos;
+
+	while (begin < end) {
+		const uint16_t tmp = *begin;
+
+		*begin = *(end - 1);
+		*(end - 1) = tmp;
+		++begin;
+		--end;
+	}
+
+	return pos;
+}
+
+static uint16_t *efi_guid_to_str(const struct efi_guid *guid, uint16_t *buf)
+{
+	uint16_t *pos = buf;
+	size_t i;
+
+	*pos++ = u'{'; *pos++ = u' ';
+	pos = u64_to_str(guid->data1, pos);
+	*pos++ = u','; *pos++ = u' ';
+	pos = u64_to_str(guid->data2, pos);
+	*pos++ = u','; *pos++ = u' ';
+	pos = u64_to_str(guid->data3, pos);
+	*pos++ = u','; *pos++ = u' ';
+	*pos++ = u'{'; *pos++ = u' ';
+	pos = u64_to_str(guid->data4[0], pos);
+	for (i = 1; i < 8; ++i) {
+		*pos++ = u','; *pos++ = u' ';
+		pos = u64_to_str(guid->data4[i], pos);
+	}
+	*pos++ = u' '; *pos++ = u'}';
+	*pos++ = u' '; *pos++ = u'}';
+
+	return pos;
+}
+
+static efi_status_t efi_guid_print(
+	struct efi_simple_text_output_protocol *out,
+	const struct efi_guid *guid)
+{
+	uint16_t buf[128];
+
+	*efi_guid_to_str(guid, buf) = 0;
+	return out->output_string(out, buf);
+}
+
+static efi_status_t efi_handle_explore(
+	struct efi_system_table *system_table,
+	efi_handle_t handle)
+{
+	uint16_t rn[] = u"\r\n";
+	efi_status_t status, other;
+	struct efi_guid **guids;
+	efi_uint_t count;
+	size_t i;
+
+	status = system_table->boot->protocols_per_handle(
+		handle, &guids, &count);
+	if (status != 0)
+		return status;
+
+	for (i = 0; i < count; ++i) {
+		status = efi_guid_print(system_table->out, guids[i]);
+		if (status != 0)
+			break;
+		status = system_table->out->output_string(
+			system_table->out, rn);
+		if (status != 0)
+			break;
+	}
+	other = system_table->boot->free_pool(guids);
+	return status != 0 ? status : other;
+}
+
 efi_status_t efi_main(
 	efi_handle_t handle, struct efi_system_table *system_table)
 {
-	uint16_t msg[] = u"Hello World!";
 	efi_status_t status;
 
 	status = system_table->out->clear_screen(system_table->out);
 	if (status != 0)
 		return status;
 
-	status = system_table->out->output_string(system_table->out, msg);
+	status = efi_handle_explore(system_table, handle);
 	if (status != 0)
 		return status;
 
